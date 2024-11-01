a1ca47c4f50792b9d7a17a9344978c0f0a60cb04
Waseem khan <wasim5388@gmail.com>
2024-11-01 19:08:33 +0500

    env variable name update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./src/utils/schema.ts",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: "postgresql://ai-mock-interviewer_owner:2EsVSeCnG6wy@ep-spring-forest-a5nqkhy6.us-east-2.aws.neon.tech/ai-mock-interviewer?sslmode=require",
   },
 });
